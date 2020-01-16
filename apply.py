46d20429e66889d1531abc99681cf04f9c54da7d
oogutierrez <OOGUTIERREZ@YAHOO.COM>
2020-01-16 17:52:37 -0600

    Create apply.py

---
+++ apply.py
@@ -0,0 +1,19 @@
+# This is for my application for Software Engineer - Big Data in FlightAware 
+
+import requests
+import json
+
+url = 'https://flightaware.com/about/careers/apply?data='
+headers = {'Content-type': 'application/json'}
+urls = [ 
+         "https://www.linkedin.com/in/orlando-gutierrez-09995a2/",
+       ]
+       
+data = {'name': 'Orlando Gutierrez',
+        'email': 'oogutierrez@yahoo.com',
+        'position': 'Software Engineer - Big Data',
+        'urls': [ urls ],
+        'comment': 'Please call me at 8323788532 or 4046617285'}
+
+r = requests.post(url + json.dumps(data), headers=headers)
+print(r.status_code, r.text)
